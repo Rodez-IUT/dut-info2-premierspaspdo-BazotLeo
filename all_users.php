4e4e7e853738beef316086f39f80b64bec7e9b3e
LeoB <leo.bazot@iut-rodez.fr>
2019-11-13 10:31:48 +0100

    fin sceance

--- all_users.php
+++ all_users.php
@@ -37,27 +37,34 @@
 		throw new PDOException($e->getMessage(), (int)$e->getCode());
 	}
 
-	$users = array();
-	$id = array();
-	$email = array();
-	$status = array();
 
-	$stmt = $pdo->query('SELECT * FROM users ORDER BY username');
+	/* Perso
+	*$user_id = array();
+	*$id = array();
+	*$email = array();
+	*$status = array();
+	*
+	*$stmt = $pdo->query('SELECT * FROM users ORDER BY username');
+	*
+	*while ($row = $stmt->fetch()) {
+	*	array_push($users, $row['username']);
+	*	array_push($id, $row['id']);
+	*	array_push($email, $row['email']);
+	*}
+	*
+	*$stmt = $pdo->query('SELECT name FROM status JOIN users ON status_id = status.id ORDER BY username');
+	*
+	*while ($row = $stmt->fetch()) {
+	*	array_push($status, $row['name']);
+	*}
+	*/
 
-	while ($row = $stmt->fetch()) {
-		array_push($users, $row['username']);
-		array_push($id, $row['id']);
-		array_push($email, $row['email']);
-	}
-
-	$stmt = $pdo->query('SELECT name FROM status JOIN users ON status_id = status.id ORDER BY username');
-
-	while ($row = $stmt->fetch()) {
-		array_push($status, $row['name']);
-	}
-
-	// $user_id = array();
-	// Correction : $stmt = $pdo->query('select users.id as user_id, username, email, s.name as status from users join status s on users.status_id = s.id');
+	$stmt = $pdo->query('select users.id as user_id, username, email, s.name as status 
+						from users u
+						join status s 
+						on u.status_id = s.id 
+						WHERE s.id = 2 AND u.username LIKE 'e%'
+						ORDER BY username');
 ?>
 
 <body>
@@ -69,26 +76,25 @@
 			<th>Status</th>
 
 	<?php
-		for ($i=0; $i < count($users); $i++) { 
-			echo '<tr>';
-			echo '<td>'.$id[$i].'</td> <td>'.$users[$i].'</td> <td>'.$email[$i].'</td> <td>'.$status[$i].'</td>';
-			echo '</tr>';
-		}
-
-		/* Correction
-		* <?php while ($row = $stmt->fetch()) { ?>
-		*	<tr>
-		*		<td><?php echo $row['user_id']?></td>
-		*		<td><?php echo $row['username']?></td>
-		*		<td><?php echo $row['email']?></td>
-		*		<td><?php echo $row['status']?></td>
-		*	</tr>
-		* <?php } ?>
+		/* Perso
+		*for ($i=0; $i < count($users); $i++) { 
+		*	echo '<tr>';
+		*	echo '<td>'.$id[$i].'</td> <td>'.$users[$i].'</td> <td>'.$email[$i].'</td> <td>'.$status[$i].'</td>';
+		*	echo '</tr>';
+		*}
 		*/
-		
-		echo "</tr>";
-		echo '</table>';
 	?>
+
+		<?php while ($row = $stmt->fetch()) { ?>
+			<tr>
+				<td><?php echo $row['user_id']?></td>
+				<td><?php echo $row['username']?></td>
+				<td><?php echo $row['email']?></td>
+				<td><?php echo $row['status']?></td>
+			</tr>
+		<?php } ?>
+		
+	</table>
 
 </body>
 </html>
